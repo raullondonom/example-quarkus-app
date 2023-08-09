a0088405f7b2e74caff4cb329ef3b198af5e24b7
raullondonomurillo@gmail.com <Raul Londono Murillo>
2023-08-08 20:52:16 -0500

    Updating proyect structure'

--- applications/app-service/src/main/java/co/com/rale/examplequarkusapp/config/UseCasesConfig.java
+++ applications/app-service/src/main/java/co/com/rale/examplequarkusapp/config/UseCasesConfig.java
@@ -1,11 +1,11 @@
-package co.com.rale.restgeneratorcli.config;
+package co.com.rale.examplequarkusapp.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(basePackages = "co.com.bancolombia.usecase",
+@ComponentScan(basePackages = "co.com.rale.examplequarkusapp.usecase",
         includeFilters = {
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
         },
